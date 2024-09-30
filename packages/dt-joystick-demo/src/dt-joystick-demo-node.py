600e5d0e305730b3472261ca114d54d330e37ca8
Kevin Lessard <kevinlessard74@gmail.com>
2024-09-30 17:44:47 -0400

    joystick demo

---
+++ packages/dt-joystick-demo/src/dt-joystick-demo-node.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import rospy
+from sensor_msgs.msg import Joy
+from duckietown_msgs.msg import WheelsCmdStamped
+
+
+class DTJoystickDemoNode:
+    def __init__(self):
+        self.sub_joy = rospy.Subscriber("/agent/joy", Joy, self.process_joy)
+        self.pub_wheels_cmds = rospy.Publisher(
+            "/agent/wheels_driver_node/wheels_cmd", WheelsCmdStamped
+        )
+
+    def process_joy(self, msg):
+        cmd_to_publish = WheelsCmdStamped()
+
+        # header contains some meta information, good practice to copy it
+        cmd_to_publish.header = msg.header
+
+        # assign the velocity from the joystick to the wheels, no movement
+        cmd_to_publish.vel_right = 0
+        cmd_to_publish.vel_left = 0
+
+        v = 0.5
+
+        if msg.axes[1] == 1.0:
+            # go forward
+            cmd_to_publish.vel_right = v
+            cmd_to_publish.vel_left = v
+
+        if msg.axes[1] == -1.0:
+            # go backward
+            cmd_to_publish.vel_right = -v
+            cmd_to_publish.vel_left = -v
+
+        if msg.axes[3] == -1.0:
+            # turn left
+            cmd_to_publish.vel_right = -v
+            cmd_to_publish.vel_left = v
+
+        if msg.aces[3] == 1.0:
+            # turn right
+            cmd_to_publish.vel_right = v
+            cmd_to_publish.vel_left = -v
+
+        # publish the message
+        self.pub_wheels_cmds.publish(cmd_to_publish)
+
+
+if __name__ == "__main__":
+    # Initialize the node
+    node = DTJoystickDemoNode()
+    rospy.init_node("dt-joystick-demo-node")
+
+    # Keep it spinning
+    rospy.spin()
